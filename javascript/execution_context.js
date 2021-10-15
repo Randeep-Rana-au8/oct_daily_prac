a1f32dc9334ab9fdd09aa7173cbaeede90edc500
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-10-15 14:50:29 +0530

    learning about Exectution context

---
+++ javascript/execution_context.js
@@ -0,0 +1,10 @@
+var n = 2;
+function getSquare(n) {
+  var square = n * n;
+  return square;
+}
+
+var squareOf2 = getSquare(n);
+var squareOf5 = getSquare(5);
+console.log(squareOf2);
+console.log(squareOf5);
